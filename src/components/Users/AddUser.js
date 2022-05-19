468c745f65b82d44097c8d3eb71b62ca3093ad54
unknown <lukacc008@gmail.com>
2022-05-19 15:30:02 +0200

    Added Card component with css.module styling

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,11 +1,15 @@
 import React from "react";
 
+import Card from "../UI/Card";
+import classes from './AddUser.module.css';
+
  const AddUser = (props) => {
    const addUserHandler = (event) => {
       event.preventDefault();
     };
 
   return (
+    <Card className={classes.input}>
    <form onSubmit={addUserHandler}>
        <label htmlFor="username">Username</label>
        <input type="text" id="username" />
@@ -13,6 +17,7 @@ import React from "react";
        <input type="number" id="age" />
        <button type="submit">Add User</button>
    </form>
+    </Card>
   );
 };
 
